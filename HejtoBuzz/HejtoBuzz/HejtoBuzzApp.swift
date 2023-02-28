022c3f416f9e0e64e1c1f935e34615978d82a71b
Lukasz <stocki.lukasz@gmail.com>
2023-02-28 22:33:27 +0100

    Initial empty project.

---
+++ HejtoBuzz/HejtoBuzz/HejtoBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  HejtoBuzzApp.swift
+//  HejtoBuzz
+//
+//  Created by Edward Maliniak on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct HejtoBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
