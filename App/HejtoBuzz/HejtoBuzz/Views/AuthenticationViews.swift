2f203f8bb5f84a3b3e218f9f6fa22063bc134381
Lukasz <stocki.lukasz@gmail.com>
2023-03-01 21:27:14 +0100

    Feature/rest client (#1)
    * Adds package.
    
    * Removes package.
    
    * More git ignore stuff.
    
    * Adds domain package.
    
    * Adds auth view.
    
    * Adds optional API.
    
    * Sorts by type.

---
+++ App/HejtoBuzz/HejtoBuzz/Views/AuthenticationViews.swift
@@ -0,0 +1,60 @@
+
+import Foundation
+import AuthenticationServices
+import SwiftUI
+import OptionalAPI
+
+struct AuthenticationView: View {
+
+    @Environment(\.webAuthenticationSession)
+    var session
+
+//    @StateObject private var model: Int = 42
+
+    @State var result: String? = .none
+    @State var showError: Bool = false
+    @State var processing: Bool = false
+
+
+    var body: some View {
+        List {
+            Section {
+                Button {
+                    Task {
+                        do {
+                            let result = try await session.authenticate(
+                                using: URL(string: Secrets.API.authUrl.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)! )!
+                                                                            ,
+                                callbackURLScheme: "hejtobuzz",
+
+                                preferredBrowserSession: .ephemeral
+                            )
+
+
+                            // TODO: - get code from url
+
+                            // TODO: - make token request
+
+                            print("🛤️", result)
+
+                        } catch {
+                            // TODO: Handle error
+                            dump(error)
+                            self.showError = true
+                        }
+                    }
+
+                } label: {
+                    Text("Authorise")
+                }
+            }
+
+            if self.result.isSome {
+                Text("🎉").font(.title)
+            } else {
+                ProgressView().progressViewStyle(.circular)
+            }
+        }
+    }
+}
+
