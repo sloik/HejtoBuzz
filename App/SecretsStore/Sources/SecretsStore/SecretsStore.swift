b0311649dfa9a81088f8375bbed5bd675a56cc64
Lukasz <stocki.lukasz@gmail.com>
2023-03-04 22:49:22 +0100

    Initial interface.

--- App/SecretsStore/Sources/SecretsStore/SecretsStore.swift
+++ App/SecretsStore/Sources/SecretsStore/SecretsStore.swift
@@ -1,6 +1,21 @@
-public struct SecretsStore {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+/// Namespace for keys that define secrets that the app can access to.
+public enum Keys {
+
+    /// Keys related to hejto developer portal.
+    public enum DeveloperPortal {
+        // Client id in the developer portal
+        case clientId
+
+        // Authentication url that should be used in OAuth
+        case authenticationString
     }
+}
+
+/// Interface on how to get values from "Secrets".
+///
+/// Secret is something that the app may need byt it should
+/// not be in the repository!!!.
+public protocol SecretsStoreType {
+    func value(for key: Keys.DeveloperPortal) -> String
 }
