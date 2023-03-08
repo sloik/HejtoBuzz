02c57226440ef3d20c605a26376151b85df5db32
Lukasz <stocki.lukasz@gmail.com>
2023-03-08 20:29:18 +0100

    Moves secrets storage in to features.

--- App/Domain/Sources/Domain/Features/FeaturesFactory.swift
+++ App/Domain/Sources/Domain/Features/FeaturesFactory.swift
@@ -1,31 +1,40 @@
 
 import Foundation
+import SecretsStore
 
 public struct FeaturesFactory {
     var _inMemoryStore: InMemoryStore
+    var _secureStore: SecretsStoreType
 
     init(
+        secureStore: SecretsStoreType,
         inMemoryStore: FeaturesFactory.InMemoryStore
     ) {
+        self._secureStore = secureStore
         self._inMemoryStore = inMemoryStore
     }
 }
 
 extension FeaturesFactory {
     var inMemoryStore: InMemoryStore { _inMemoryStore }
+
+    /// Gets current secret store.
+    public var secureStore: SecretsStoreType { _secureStore }
 }
 
 // MARK: - Mock
 
 extension FeaturesFactory {
-    static var prod: Self {
+    static func prod(secureStore: SecretsStoreType) -> Self {
         .init(
+            secureStore: secureStore,
             inMemoryStore: .prod
         )
     }
 
     static var mock: Self {
         .init(
+            secureStore:  MockSecureStore(),
             inMemoryStore: .mock
         )
     }
--- App/Domain/Sources/Domain/World.swift
+++ App/Domain/Sources/Domain/World.swift
@@ -5,25 +5,20 @@ public private(set) var Current = World.mock
 
 public struct World {
 
-    var _secureStore: SecretsStoreType
+
     var _useCases: UseCaseFactory
     var _features: FeaturesFactory
 
     init(
-        secureStore: SecretsStoreType,
         useCases: UseCaseFactory,
         features: FeaturesFactory
     ) {
-        self._secureStore = secureStore
         self._useCases = useCases
         self._features = features
     }
 }
 
 public extension World {
-
-    /// Gets current secret store.
-    var secureStore: SecretsStoreType { _secureStore }
 
     var useCases: UseCaseFactory { _useCases }
 
@@ -36,15 +31,13 @@ public extension World {
 
     static func takeOff(secureStore: SecretsStoreType) {
         Current = World(
-            secureStore: secureStore,
             useCases: .prod,
-            features: .prod
+            features: .prod(secureStore: secureStore)
         )
     }
 
     static var mock: World {
         .init(
-            secureStore: MockSecureStore(),
             useCases: .mock,
             features: .mock
         )
