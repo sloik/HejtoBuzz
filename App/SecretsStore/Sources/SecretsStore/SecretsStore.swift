6e4e4e0c1b91d5c4138161d65d3df8a41ce32290
Lukasz <stocki.lukasz@gmail.com>
2023-03-11 10:38:48 +0100

    Adds documentation and nice API to secrets store.

--- App/SecretsStore/Sources/SecretsStore/SecretsStore.swift
+++ App/SecretsStore/Sources/SecretsStore/SecretsStore.swift
@@ -1,3 +1,4 @@
+import Foundation
 
 /// Namespace for keys that define secrets that the app can access to.
 public enum Keys {
@@ -15,6 +16,9 @@ public enum Keys {
 
         // Authentication url that should be used in OAuth
         case authenticationString
+
+        // Scheme used for callback from OAuth
+        case callbackScheme
     }
 }
 
@@ -26,12 +30,51 @@ public protocol SecretsStoreType {
     func value(for key: Keys.DeveloperPortal) -> String
 }
 
+// MARK: - Errors
+
+/// Errors that can be thrown by `SecretsStoreType`.
+public enum SecretsStoreError: Error {
+
+    /// Thrown when the url is invalid.
+    case invalidURL(String)
+}
+
+
+// MARK: - Nicer API
+
+public extension SecretsStoreType {
+
+    /// String used for creating authentication URL.
+    var authenticationString: String { value(for: .authenticationString) }
+
+    /// Scheme used by the OAuth for callback.
+    var callbackScheme: String { value(for: .callbackScheme) }
+
+    /// URL used for authentication.
+    /// - `throws`: `SecretsStoreError.invalidURL` if the url is invalid.
+    var authenticationURL: URL {
+         get throws {
+            guard
+                let url: URL = authenticationString
+                    .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                    .flatMap( URL.init(string:) )
+             else {
+                throw SecretsStoreError.invalidURL(authenticationString)
+            }
+
+            return url
+        }
+    }
+}
+
 // MARK: - Mock
 
+/// Mock implementation of `SecretsStoreType`.
 public final class MockSecureStore: SecretsStoreType {
 
     public init(){}
-    
+
+    /// Returns "not implemented" for all keys.
     public func value(for key: Keys.DeveloperPortal) -> String {
         "not implemented"
     }
