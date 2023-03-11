b725e107e4f6e6ca9ece5f7bdd33c6d0732f4904
Edward Maliniak <mail@mail.com>
2023-03-12 00:10:32 +0100

    Adds dependency to tests.

--- App/HejDTO/Package.swift
+++ App/HejDTO/Package.swift
@@ -15,6 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-custom-dump.git", from: "0.9.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,6 +25,10 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "HejDTOTests",
-            dependencies: ["HejDTO"]),
+            dependencies: [
+                "HejDTO",
+                .product(name: "CustomDump", package: "swift-custom-dump"),
+            ]
+        ),
     ]
 )
