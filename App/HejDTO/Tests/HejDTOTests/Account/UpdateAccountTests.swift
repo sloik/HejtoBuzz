4e088bf36393adedcc3a2ae14e94862b25c1126d
Edward Maliniak <mail@mail.com>
2023-03-12 11:26:09 +0100

    Adds test.

---
+++ App/HejDTO/Tests/HejDTOTests/Account/UpdateAccountTests.swift
@@ -0,0 +1,38 @@
+
+import XCTest
+@testable import HejDTO
+import CustomDump
+
+final class UpdateAccountTests: XCTestCase {
+
+    // https://docs.hejto.pl/#tag/Account/operation/put_account
+    func test_parsingJsonExample() throws {
+
+        let jsonString = """
+        {
+          "email": "string",
+          "sex": "male",
+          "birthday": "2019-08-24",
+          "city": "string",
+          "description": "string",
+          "username": "string"
+        }
+        """
+
+        // Act
+        let result = try JSONDecoder().decode(UpdateAccount.self, from: jsonString.data(using: .utf8)!)
+
+        // Assert
+        let expectedResult = UpdateAccount(
+            email: .string,
+            sex: "male",
+            birthday: .date2019_08_24,
+            city: .string,
+            description: .string,
+            username: .string
+        )
+
+        XCTAssertNoDifference(result, expectedResult)
+    }
+
+}
