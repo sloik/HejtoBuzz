4bd18e77887834bcc914ccd84b5cb0e1ab377cd7
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-03-12 23:49:19 +0100

    Adds DTO and tests.

---
+++ App/HejDTO/Sources/HejDTO/Account/GET/Notifications/Account.Notification.swift
@@ -0,0 +1,62 @@
+
+import Foundation
+
+extension Account {
+
+    /// https://docs.hejto.pl/#tag/Account/operation/get_account_notifications
+    public struct Notification: Codable, Equatable {
+
+        public struct Sender: Codable, Equatable {
+
+            public struct Links: Codable, Equatable {
+                public let selfLink: Link
+                public let follows: Link
+
+                private enum CodingKeys: String, CodingKey {
+                    case selfLink = "self"
+                    case follows
+                }
+            }
+
+            public let username: String
+            public let sex: String
+            public let avatar: Image
+            public let background: Image
+            public let status: String
+            public let controversial: Bool
+            public let currentRank: String
+            public let currentColor: String
+            public let verified: Bool
+            public let sponsor: Bool
+            public let createdAt: String
+            public let links: Links
+
+            private enum CodingKeys: String, CodingKey {
+                case username
+                case sex
+                case avatar
+                case background
+                case status
+                case controversial
+                case currentRank = "current_rank"
+                case currentColor = "current_color"
+                case verified
+                case sponsor
+                case createdAt = "created_at"
+                case links = "_links"
+            }
+        }
+
+        let sender: Sender
+        let type: String
+        let resourceName: String
+        let resourceAction: String
+
+        private enum CodingKeys: String, CodingKey {
+            case sender
+            case type
+            case resourceName = "resource_name"
+            case resourceAction = "resource_action"
+        }
+    }
+}
---
+++ App/HejDTO/Tests/HejDTOTests/Account/Account.Notification.Tests.swift
@@ -0,0 +1,80 @@
+
+import XCTest
+@testable import HejDTO
+import CustomDump
+
+final class AccountNotificationTests: XCTestCase {
+
+    // https://docs.hejto.pl/#tag/Account/operation/put_account
+    func test_parsingJsonExample() throws {
+
+        let jsonString = """
+            {
+              "sender": {
+                "username": "string",
+                "sex": "string",
+                "avatar": {
+                  "alt": "string",
+                  "uuid": "string"
+                },
+                "background": {
+                  "alt": "string",
+                  "uuid": "string"
+                },
+                "status": "string",
+                "controversial": true,
+                "current_rank": "string",
+                "current_color": "string",
+                "verified": true,
+                "sponsor": true,
+                "created_at": "2019-08-24T14:15:22Z",
+                "_links": {
+                  "self": {
+                    "href": "string"
+                  },
+                  "follows": {
+                    "href": "string"
+                  }
+                }
+              },
+              "type": "string",
+              "resource_name": "string",
+              "resource_action": "string"
+            }
+            """
+
+        // Act
+        let result = try JSONDecoder()
+            .decode(
+                Account.Notification.self,
+                from: jsonString.data(using: .utf8)!
+            )
+
+        // Assert
+        let expectedResult = Account.Notification(
+            sender: Account.Notification.Sender(
+                username: .string,
+                sex: .string,
+                avatar: Image(alt: .string, uuid: .string),
+                background: Image(alt: .string, uuid: .string),
+                status: .string,
+                controversial: true,
+                currentRank: .string,
+                currentColor: .string,
+                verified: true,
+                sponsor: true,
+                createdAt: .date2019_08_24T141522Z,
+                links: Account.Notification.Sender.Links(
+                    selfLink: Link(href: .string),
+                    follows: Link(href: .string)
+                )
+            ),
+            type: .string,
+            resourceName: .string,
+            resourceAction: .string
+        )
+
+        XCTAssertNoDifference(result, expectedResult)
+    }
+
+}
