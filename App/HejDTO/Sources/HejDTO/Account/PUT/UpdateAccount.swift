827f93ef44e614cec7f3b616e46751aef436ce0b
Edward Maliniak <mail@mail.com>
2023-03-12 11:13:10 +0100

    Adds DTO.

---
+++ App/HejDTO/Sources/HejDTO/Account/PUT/UpdateAccount.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+// https://docs.hejto.pl/#tag/Account/operation/put_account
+struct UpdateAccount: Codable, Equatable {
+    let email: String
+    let sex: String
+    let birthday: String
+    let city: String
+    let description: String
+    let username: String
+}
