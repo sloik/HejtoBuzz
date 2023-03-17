beac950837c7b1debd06183e4ea7fcaa90799e85
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-03-17 23:37:57 +0100

    Adds documentation.

--- App/HejDTO/Sources/HejDTO/Links/POST/CreateLink.swift
+++ App/HejDTO/Sources/HejDTO/Links/POST/CreateLink.swift
@@ -2,6 +2,8 @@
 import Foundation
 
 extension Links {
+
+    /// https://docs.hejto.pl/#tag/Links/operation/post_link
     public struct CreateLink: Codable, Equatable {
         public let url: String
     }
