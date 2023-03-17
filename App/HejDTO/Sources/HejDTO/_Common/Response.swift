b75260c8fd33034ab9c13f58a891b612aa794026
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-03-17 23:22:45 +0100

    Adds documentation.

--- App/HejDTO/Sources/HejDTO/_Common/Response.swift
+++ App/HejDTO/Sources/HejDTO/_Common/Response.swift
@@ -4,6 +4,15 @@ import Foundation
 /// Namespace for common responses.
 public enum Response {
 
+    /// Response with a message.
+    ///
+    /// Example of json payload for this object:
+    ///
+    /// ```json
+    /// {
+    ///    "message": "Response text"
+    /// }
+    /// ```
     struct Message: Codable, Equatable {
         public let message: String
     }
