f2ae0a0a2e7c822b84c40fb724eb5a84faa711d9
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-03-17 20:59:51 +0100

    Adds DTO.

---
+++ App/HejDTO/Sources/HejDTO/Account/GET/Warnings/Account.Warning.swift
@@ -0,0 +1,65 @@
+
+
+import Foundation
+
+extension Account {
+
+    /// https://docs.hejto.pl/#tag/Account/operation/get_account_warning
+    public struct Warning: Codable, Equatable {
+
+        public struct Author: Codable, Equatable {
+            public let username: String
+            public let sex: String
+            public let avatar: Image
+            public let background: Image
+            public let status: String
+            public let controversial: Bool
+            public let currentRank: String
+            public let currentColor: String
+            public let verified: Bool
+            public let sponsor: Bool
+            public let createdAt: String
+
+            public struct Links: Codable, Equatable {
+                public let selfLink: Link
+                public let follows: Link
+
+                private enum CodingKeys: String, CodingKey {
+                    case selfLink = "self"
+                    case follows
+                }
+            }
+            public let links: Links
+
+            private enum CodingKeys: String, CodingKey {
+                case username, sex, avatar, background, status, controversial
+                case currentRank = "current_rank"
+                case currentColor = "current_color"
+                case verified, sponsor
+                case createdAt = "created_at"
+                case links = "_links"
+            }
+        }
+        public let author: Author
+
+        public struct Links: Codable, Equatable {
+            public let selfLink: Link
+
+            private enum CodingKeys: String, CodingKey {
+                case selfLink = "self"
+            }
+        }
+        public let links: Links
+
+        public let reason: String
+        public let uuid: String
+        public let createdAt: String
+
+        
+        private enum CodingKeys: String, CodingKey {
+            case reason, author, uuid
+            case createdAt = "created_at"
+            case links = "_links"
+        }
+    }
+}
