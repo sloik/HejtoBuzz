dd14f0e2dc424c6d3bfca3fa103766e2e91fba9d
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-05-06 16:33:29 +0200

    Extracts "Codable" to extension and adds "Equatable" conformance.

--- App/HejDTO/Sources/HejDTO/_Common/Response.swift
+++ App/HejDTO/Sources/HejDTO/_Common/Response.swift
@@ -44,24 +44,27 @@ public extension Response {
     ///  }
     ///  ```
     @dynamicMemberLookup
-    struct Errors: Codable {
+    struct Errors: Equatable {
         let errors: [String: [String: [String]]]
 
         subscript(dynamicMember member: String) -> [String]? {
             errors["errors"]?[member]
         }
+    }
+}
 
-        public init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            var errors = [String: [String: [String]]]()
+extension Response.Errors: Codable {
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        var errors = [String: [String: [String]]]()
 
 
-            for key in container.allKeys {
-                let value = try container.decode([String: [String]].self, forKey: key)
-                errors[key.stringValue] = value
-            }
-
-            self.errors = errors
+        for key in container.allKeys {
+            let value = try container.decode([String: [String]].self, forKey: key)
+            errors[key.stringValue] = value
         }
+
+        self.errors = errors
     }
 }
