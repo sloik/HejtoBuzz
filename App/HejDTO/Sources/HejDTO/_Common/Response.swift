4ffcaaf2314cd5a190574fa64e1300678da69f33
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-05-06 16:24:44 +0200

    Adds errors response.

--- App/HejDTO/Sources/HejDTO/_Common/Response.swift
+++ App/HejDTO/Sources/HejDTO/_Common/Response.swift
@@ -4,6 +4,12 @@ import Foundation
 /// Namespace for common responses.
 public enum Response {
 
+}
+
+// MARK: - Message
+
+public extension Response {
+
     /// Response with a message.
     ///
     /// Example of json payload for this object:
@@ -16,5 +22,46 @@ public enum Response {
     struct Message: Codable, Equatable {
         public let message: String
     }
+}
 
+// MARK: - Errors
+
+public extension Response {
+
+    /// Possible errors returned from many endpoints.
+    ///
+    /// Example of json payload:
+    /// ```json
+    /// {
+    ///    "errors": {
+    ///      "field_name": [
+    ///        "This field cannot be empty"
+    ///      ],
+    ///      "another_field_name": [
+    ///        "This field should be a number"
+    ///      ]
+    ///    }
+    ///  }
+    ///  ```
+    @dynamicMemberLookup
+    struct Errors: Codable {
+        let errors: [String: [String: [String]]]
+
+        subscript(dynamicMember member: String) -> [String]? {
+            errors["errors"]?[member]
+        }
+
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            var errors = [String: [String: [String]]]()
+
+
+            for key in container.allKeys {
+                let value = try container.decode([String: [String]].self, forKey: key)
+                errors[key.stringValue] = value
+            }
+
+            self.errors = errors
+        }
+    }
 }
