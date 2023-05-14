e9593275cf32c06138a0b69f2d855f0d38e635f0
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-05-14 11:55:18 +0200

    Fixes comments.

--- App/HejDTO/Sources/HejDTO/Tags/POST/Tags.PostBlocks.swift
+++ App/HejDTO/Sources/HejDTO/Tags/POST/Tags.PostBlocks.swift
@@ -1,7 +1,7 @@
 
 extension Tags {
 
-    /// https://docs.hejto.pl/#tag/Tags/operation/post_tag_follow
+    /// https://docs.hejto.pl/#tag/Tags/operation/post_tag_block
     /// No DTO, just empty namespace.
     public enum PostBlocks {
 
--- App/HejDTO/Sources/HejDTO/Tags/POST/Tags.PostFollows.swift
+++ App/HejDTO/Sources/HejDTO/Tags/POST/Tags.PostFollows.swift
@@ -1,7 +1,7 @@
 
 extension Tags {
 
-    /// https://docs.hejto.pl/#tag/Tags/operation/post_tag_block
+    /// https://docs.hejto.pl/#tag/Tags/operation/post_tag_follow
     /// No DTO, just empty namespace.
     public enum PostFollows {
 
