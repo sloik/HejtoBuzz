5c77ff94b75b954c3008b9e91c14e65581ddfca8
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-05-14 11:48:18 +0200

    Fixes types and names.

--- App/HejDTO/Sources/HejDTO/Tags/POST/Tags.PostBlocks.swift
+++ App/HejDTO/Sources/HejDTO/Tags/POST/Tags.PostBlocks.swift
@@ -3,7 +3,7 @@ extension Tags {
 
     /// https://docs.hejto.pl/#tag/Tags/operation/post_tag_follow
     /// No DTO, just empty namespace.
-    public enum PostFollows {
+    public enum PostBlocks {
 
     }
 }
--- App/HejDTO/Sources/HejDTO/Tags/POST/Tags.PostFollows.swift
+++ App/HejDTO/Sources/HejDTO/Tags/POST/Tags.PostFollows.swift
@@ -3,7 +3,7 @@ extension Tags {
 
     /// https://docs.hejto.pl/#tag/Tags/operation/post_tag_block
     /// No DTO, just empty namespace.
-    public enum PostBlocks {
+    public enum PostFollows {
 
     }
 }
