39e0ddf0bc8caf09f08e75fce33c7408fe2616e9
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-05-19 21:54:05 +0200

    Marks empty DTOs.

---
+++ App/HejDTO/Sources/HejDTO/Communities/POST/Communities.PostBlock.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+extension Communities {
+
+    /// https://docs.hejto.pl/#tag/Communities/operation/post_community_block
+    /// empty DTO
+    enum PostBlock {
+
+    }
+
+}
