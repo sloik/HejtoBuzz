fa98da6aa81f16a975deea7a5062a35df31bd0ce
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-05-19 22:36:48 +0200

    Removes comment.

--- App/HejDTO/Sources/HejDTO/Communities/PATCH/Communities.UpdateCommunityMember.swift
+++ App/HejDTO/Sources/HejDTO/Communities/PATCH/Communities.UpdateCommunityMember.swift
@@ -4,7 +4,6 @@ import Foundation
 extension Communities {
 
     /// https://docs.hejto.pl/#tag/Communities/operation/patch_community_member
-    /// empty DTO
     struct UpdateCommunityMember: Codable, Equatable {
         public let status: String
         public let role: String
